327fc3e64a3bd3eea9a9e5b0c65021c4dfff5257
Cal Colistra <calcolistra10@gmail.com>
2022-10-26 20:16:21 -0400

    format comments

--- src/index.js
+++ src/index.js
@@ -16,38 +16,40 @@ const firebaseConfig = {
     measurementId: "G-YL4JNFK07K"
   };
 
-  //init firebase app
+//init firebase app:
 initializeApp(firebaseConfig)
 
-//init services
+//init services:
 const db = getFirestore()
 
-//collection ref:
+//init collection ref:
 const colRef = collection(db, 'Users')
 
 //queries
 const q = query(colRef, where("userName", "==", "user1"))
 
-  //get collection data from query above:
-  onSnapshot(q, (snapshot) => {
-    let users = []
-    snapshot.docs.forEach((doc) => {
-      users.push({ ...doc.data(), id: doc.id })
-    })
-    console.log(users)
+//get collection data from query above:
+let users = []
+onSnapshot(q, (snapshot) => {
+  snapshot.docs.forEach((doc) => {
+    users.push({ ...doc.data(), id: doc.id })
   })
- 
-  //get a single document:
-  const docRef = doc(db, 'Users', 'user1')
-  
-  onSnapshot(docRef, (doc) => {
-    console.log(doc.data(), doc.id)
-  })
+  console.log(users)  //print data in users[] to console
+})
+//attempt to write data in users[] in html (doesn't work):
+document.getElementById("test").innerHTML = "" + users[0];  //undefined
 
-  //get a single document:
-  const docRef2 = doc(db, 'user1Classes', 'Calculus')
-  
-  onSnapshot(docRef2, (doc) => {
-    console.log(doc.data(), doc.id)
-  })
-  //const docRef = doc(db, 'Users')
+//get a single document:
+const docRef = doc(db, 'Users', 'user1')
+//print user1's data to the console:
+onSnapshot(docRef, (doc) => {
+  console.log(doc.data(), doc.id)
+})
+
+//get a single document (doesn't work):
+const docRef2 = doc(db, 'user1Classes', 'Calculus')
+//print user1Classes data to console (Calculus is undefined):
+onSnapshot(docRef2, (doc) => {
+  console.log(doc.data(), doc.id)
+})
+//const docRef = doc(db, 'Users')
