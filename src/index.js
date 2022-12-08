c02e64fc882dc910d4eea381cec2956a12add6a3
Cal Colistra <calcolistra10@gmail.com>
2022-12-07 22:23:43 -0500

    added updateScans() and updateClasses() fucntions

--- src/index.js
+++ src/index.js
@@ -33,6 +33,15 @@ let showTable = document.getElementById("showTable");
 let showClasses = document.getElementById("showClasses");
 //  init reference to element with id currnetClass
 let currentClass = document.getElementById("currentClass").textContent;
+// array of users:
+const allUsers = ['20fbc924', '2183fc1d'];
+//  variable for how early one can swipe into a class:
+let earlySwipe = 35;  // 35 minutes
+earlySwipe = earlySwipe * 60000 +1000;  // convert to milliseconds
+//  variable for how late one can swipe into class and still be marked on time:
+let lateSwipe = 5;  //  5 minutes
+lateSwipe = lateSwipe *60000 +1000;
+let test = true;
 //------------------------------------------------------------------
 //  function that executes every .3 of a second:
 setInterval(function() { // run every half second
@@ -86,7 +95,194 @@ setInterval(function() { // run every half second
     //  set addStudentBool to false:
     document.getElementById("addStudentBool").innerText = "false";
   }
+  //updateScans();
+  if (test) {
+    updateClasses();
+    test = false;
+  }
 }, 300);   //time interval for every half second (300ms = .3 seconds)
+//------------------------------------------------------------------
+//  function that updates the RFID_scans array for students subcollections
+//  that exist in specific class documents
+async function updateClasses() {
+  for (let i = 0; i < allUsers.length; i++) {  //  iterate through users
+    let user = allUsers[i];  //  get current user
+    const userRef = doc(db, "Users", user);  //  make a reference to Users collection
+    const userSnap = await getDoc(userRef);  //snapshot data
+    let userData = [];  //  array to be given user's data
+    if (userSnap.exists()) {  //  check if snapshot exists
+      //  push user's data from db to array:
+      userData.push(userSnap.data());
+      //console.log("Document data:", userData);
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+    let userClasses = [];  //  array to be given the current user's classes
+    userClasses.push(userData[0].Classes);  //  push classes to userClasses[]
+    let allScans = [];  //  array to be given current user's scans
+    allScans.push(userData[0].allScans);  //  push scans to allScans[]
+    //  iterate through current user's classes:
+    for (let i = 0; i < userClasses[0].length; i++) {
+      let currentClass = userClasses[0][i];  //  get current class to work with
+      //console.log("NEW CLASS: "+currentClass );  //  logging for debugging
+      //  make a reference to the user's subcollection in the current class
+      const classRef = doc(db, "Classes", currentClass, "Students", user);
+      const classSnap = await getDoc(classRef);  //snapshot data
+      let meetings = [];  //  array to be given user's meetings
+      if (classSnap.exists()) {  //  check if snapshot exists
+        meetings.push(classSnap.data());  //  push data to meetings[]
+        //console.log("Document data:", meetings);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+      //  make a reference to classes collection:
+      const timeRef = doc(db, "Classes", currentClass);
+      const timeSnap = await getDoc(timeRef);  //snapshot data
+      let times = [];  //  array to be given class's data
+      if (classSnap.exists()) {  //  check if snapshot exists
+        times.push(timeSnap.data());  //  push data to time[]
+        //console.log("start&end times:", times);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+      let startTime = times[0].startTime;  //  get class's starting time
+      let endTime = times[0].endTime;  //  get class's starting time
+      let startDay = times[0].startDate;  //  get class's starting date
+      let endDay = times[0].endDate;  //  get class's ending date
+      //console.log(startTime, endTime, startDay,endDay);
+      //  convert from firebase timestamp to Date object:
+      let startDate = new Date(startDay);
+      //  convert from firebase timestamp to Date object:
+      let endDate = new Date(endDay);
+      startDate.setHours(0);  //  set hours to 0
+      endDate.setHours(0);  //  set hours to 0
+      //console.log(allScans);
+
+      let userSwipes = [];  //  array to be given user's swipes
+      //  iterate through allScans[]:
+      for (let i=0; i < allScans[0].length; i++) {
+        let fbTS = allScans[0][i];  //  get firebase timestamp
+        let aDate = new Date(fbTS.toDate());  //  convert to date object
+        userSwipes.push(aDate);  //  push date to userSwipes
+      }
+      let userMeetings = [];  //  array to be given user meetings
+      //  iterate through RFID_scans array from db:
+      for (let i=0; i < meetings[0].RFID_scans.length; i++) {
+        //  get the firebase timestamp:
+        let fbTS = meetings[0].RFID_scans[i];
+        let aDate = new Date(fbTS.toDate());  // convert to date object
+        userMeetings.push(aDate);  //  push to userMeetings[]
+      }
+      //  logs for debugging data:
+      /*
+      console.log("userMeetings[]: " + userMeetings);
+      console.log("userSwipes[]: " + userSwipes);
+      console.log("startDate: " + startDate);
+      console.log("endDate: " + endDate);
+      console.log("startTime: " + startTime);
+      console.log("endTime: " + endTime);
+      */
+     //  array to hold userMeetings converter to strings:
+     let stringUserMeetings = [];
+     for (let i=0; i < userMeetings.length; i++) {  //  iterate through userMeetings
+      // convert userMeetings[i] to string & push to new array:
+      stringUserMeetings.push(String(userMeetings[i]));
+     }
+     //console.log(stringUserMeetings);
+      for (let i=0; i < userSwipes.length; i++) {  //  iterate through userSwipes[]
+        //  new variable to hold current date:
+        let checkSwipe = new Date(userSwipes[i]);
+        checkSwipe.setHours(0);  //  set hours to 0
+        checkSwipe.setMinutes(0);  //  set min to 0
+        checkSwipe.setSeconds(0);  //  set sec to 0
+        //console.log(checkSwipe+" vs "+userMeetings);  //  log for debugging
+        //  check if user has swiped on the same day as a class meeting:
+        if (stringUserMeetings.includes(String(checkSwipe))) {   
+          let swipeTime = userSwipes[i];  //  get the users swipe time
+          //  get the index of the meeting that the user has potentially swiped into:
+          let ind = stringUserMeetings.indexOf(String(checkSwipe));
+          //  init a date object for the start of this class meeting:
+          let starting = new Date(userMeetings[ind]);
+          //console.log(ind);
+          //  set the hours and minutes of starting to the start time of the class
+          starting.setHours(startTime.substring(0,2));
+          starting.setMinutes(startTime.substring(3,5));
+          //  init a date object for the end of this class meeting:
+          let ending = new Date(userMeetings[ind]);
+          //  set the hours and minutes of ending to the end time of the class
+          ending.setHours(endTime.substring(0,2));
+          ending.setMinutes(endTime.substring(3,5));
+          //  check if time of userSwipes[i] is valid for given class meeting,
+          //  check if it is early, or before the end of class:
+          if ( ((swipeTime.getTime()) >= (starting.getTime() - earlySwipe)) && 
+            (swipeTime <= ending) ) {
+              //console.log("if was true");  //  log for debugging
+              //  update string array
+              stringUserMeetings[ind] = String(userSwipes[i]);
+              //  update userMeetings[] with the valid swipe:
+              userMeetings[ind] = userSwipes[i];
+          }
+        }
+  
+      }
+      // update document in collection 'Classes' with the stamps array:
+      await updateDoc(doc(db, "Classes", currentClass, "Students", user), {
+        RFID_scans: userMeetings
+      });
+    }
+  }
+  
+}
+//------------------------------------------------------------------
+//  function that looks for new any scans and updates a log of previous scans:
+async function updateScans() {
+  //  iterate through users array:
+  for (let i = 0; i < allUsers.length; i++) {
+    let user = allUsers[i];  //  get current user
+    //  grab the users doc from'RFID_scans' collection from database:
+    const docRef = doc(db, "RFID_scans", user);
+    const docSnap = await getDoc(docRef);  //snapshot data
+    let data = [];  //array to be given doc data
+    if (docSnap.exists()) {  //  check if snapshot exists
+      data.push(docSnap.data().time_stamp);  //  push time_stamp array to data
+      //console.log("Document data:", data);  // log for debugging
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+    let sec = data[0].seconds;  //  get the seconds
+    let nanoSec = data[0].nanoseconds;  //  get the nanoseconds
+    let newStamp = new Timestamp(sec,nanoSec);  //  create a firebase timestamp
+
+    let data2 = [];  //  init data2[] to be given user data
+    const docRef2 = doc(db, "Users", user);  //  make a reference to User's collection
+    const docSnap2 = await getDoc(docRef2);  //snapshot data
+    if (docSnap2.exists()) {  //  check if snapshot exists
+      //  push most recent scan names from db to data2[]:
+      data2.push(docSnap2.data().most_recent_scan);
+      //  console.log("Document data2:", data2);  //  log for debugging
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+    //  init oldStamp var to hold the previous scan
+    let oldStamp = new Timestamp(data2[0].seconds, data2[0].nanoseconds);
+    //  check if oldStamp is not the same as the newStamp:
+    if (newStamp != oldStamp) {
+      //  if so, add it to the allScans[] array in the db:
+      setDoc(docRef2, { allScans: arrayUnion(newStamp) }, { merge: true });
+      await updateDoc(docRef2, {  //  update most_recent_scan in db
+        most_recent_scan: newStamp
+      });
+      await updateDoc(docRef, {  //  update time_stamp in db:
+        time_stamp: newStamp
+      });
+    }
+  }
+}
 //------------------------------------------------------------------
 // function that populates the home page with course names from the database:
 async function populateHomePage() {
@@ -228,6 +424,39 @@ async function populateTable(user, currentClass) {
       if (thisYear < checkYear) {  //  check if the class is next year
         timeStamps = "TBD";  //  set their time stamp to TBD
         marks = "TBD";  //  set their mark to TBD
+      }
+    }
+    else if (timeStamps != "12:00:00 AM") {  //  check if user has swiped
+      let timeRef = doc(db, "Classes", currentClass);
+      let timeSnap = await getDoc(timeRef);  //snapshot data
+      let times = [];
+      marks = "Absent";
+      if (timeSnap.exists()) {
+        times.push(timeSnap.data());
+        //console.log("start&end times:", times);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+      let startTime = times[0].startTime;
+      let endTime = times[0].endTime;
+      let starting = new Date(fireBaseTime);
+      starting.setHours(startTime.substring(0,2));
+      starting.setMinutes(startTime.substring(3,5));
+      let ending = new Date(fireBaseTime);
+      ending.setHours(endTime.substring(0,2));
+      ending.setMinutes(endTime.substring(3,5));
+      if ( ((fireBaseTime.getTime()) >= (starting.getTime() - earlySwipe)) && 
+            (fireBaseTime < starting) ) {
+              marks = "Early";
+          }
+      else if ( ((fireBaseTime.getTime()) >= starting.getTime()) && 
+            (fireBaseTime.getTime() <= starting.getTime() + lateSwipe) ) {
+              marks = "Present";
+      }
+      else if ( ((fireBaseTime.getTime()) >= starting.getTime() + lateSwipe) && 
+            (fireBaseTime.getTime() <= ending.getTime()) ) {
+              marks = "Late";
       }
     }
     //  add a table row with the time stamp data to the table string
